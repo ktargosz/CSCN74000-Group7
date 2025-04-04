90b487a4fbaf9caac6bbcfcd59f943cb3d95cefe
Bowen Zhang <Bzhang4974@conestogac.on.ca>
2025-04-04 11:10:07 -0400

    Fixed V2506: refactored main() to use single exit point

--- FlightClient/Src/client.cpp
+++ FlightClient/Src/client.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 int main() {
     WSADATA wsaData;
+    bool errorOccurred = false;
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         cerr << "[Client] WSAStartup failed!" << endl;
         return 1;
@@ -28,31 +30,28 @@ int main() {
 
     if (inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <= 0) {
         cerr << "[Client] Invalid address or inet_pton failed." << endl;
-        WSACleanup();
-        return 1;
+        errorOccurred = true;
     }
-
-    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) != 0) {
+    else if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) != 0) {
         cerr << "[Client] Failed to connect to server." << endl;
-        return 1;
-    }
-
-    cout << "[Client] Connected to server. Waiting for data..." << endl;
-
-    char buffer[2048] = {};
-    int bytes = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytes == SOCKET_ERROR) {
-        cerr << "[Client] Socket error while receiving data." << endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-    else if (bytes == 0) {
-        cerr << "[Client] Connection closed by server." << endl;
+        errorOccurred = true;
     }
     else {
-        cout << "[Client] Received flight schedule:\n";
-        cout << string(buffer, bytes) << endl;
+        cout << "[Client] Connected to server. Waiting for data..." << endl;
+
+        char buffer[2048] = {};
+        int bytes = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytes == SOCKET_ERROR) {
+            cerr << "[Client] Socket error while receiving data." << endl;
+            errorOccurred = true;
+        }
+        else if (bytes == 0) {
+            cerr << "[Client] Connection closed by server." << endl;
+        }
+        else {
+            cout << "[Client] Received flight schedule:\n";
+            cout << string(buffer, bytes) << endl;
+        }
     }
 
     cout << "[Client] Press Enter to exit...";
@@ -61,5 +60,9 @@ int main() {
 
     closesocket(clientSocket);
     WSACleanup();
+
+    if (errorOccurred) {
+        return 1;
+    }
     return 0;
 }
--- FlightServer/Src/server.cpp
+++ FlightServer/Src/server.cpp
@@ -31,12 +31,20 @@ namespace flight {
 
 int main() {
     WSADATA wsaData;
+    bool errorOccurred = false;
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         cerr << "[Server] WSAStartup failed!" << endl;
         return 1;
     }
 
     SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        cerr << "[Server] Failed to create socket." << endl;
+        WSACleanup();
+        return 1;
+    }
+
     sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(12345);
@@ -44,42 +52,43 @@ int main() {
 
     if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) != 0) {
         cerr << "[Server] Bind failed!" << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
+        errorOccurred = true;
     }
-
-    if (listen(serverSocket, 1) != 0) {
+    else if (listen(serverSocket, 1) != 0) {
         cerr << "[Server] Listen failed!" << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
+        errorOccurred = true;
+    }
+    else {
+        cout << "[Server] Server started. Waiting for client..." << endl;
+
+        sockaddr_in clientAddr{};
+        int clientLen = sizeof(clientAddr);
+        SOCKET clientSocket = accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddr), &clientLen);
+        if (clientSocket == INVALID_SOCKET) {
+            cerr << "[Server] Accept failed!" << endl;
+            errorOccurred = true;
+        }
+        else {
+            cout << "[Server] Client connected." << endl;
+
+            std::string flights = flight::loadFlightSchedule("flights.txt");
+            int sent = send(clientSocket, flights.c_str(), static_cast<int>(flights.size()), 0);
+            if (sent == SOCKET_ERROR) {
+                cerr << "[Server] Send failed!" << endl;
+                errorOccurred = true;
+            }
+            else {
+                cout << "[Server] Sent flight schedule to client." << endl;
+            }
+            closesocket(clientSocket);
+        }
     }
 
-    cout << "[Server] Server started. Waiting for client..." << endl;
-
-    sockaddr_in clientAddr{};
-    int clientLen = sizeof(clientAddr);
-    SOCKET clientSocket = accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddr), &clientLen);
-    if (clientSocket == INVALID_SOCKET) {
-        cerr << "[Server] Accept failed!" << endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-    cout << "[Server] Client connected." << endl;
-
-    // Read schedule and send to client
-    std::string flights = flight::loadFlightSchedule("flights.txt");
-    int sent = send(clientSocket, flights.c_str(), static_cast<int>(flights.size()), 0);
-    if (sent == SOCKET_ERROR) {
-        cerr << "[Server] Send failed!" << endl;
-    }
-
-    cout << "[Server] Sent flight schedule to client." << endl;
-
-    closesocket(clientSocket);
     closesocket(serverSocket);
     WSACleanup();
+
+    if (errorOccurred) {
+        return 1;
+    }
     return 0;
 }
