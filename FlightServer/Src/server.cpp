5699ad8b149485acf53a1a6ceb58171b837675fa
kenta <kenta@RAZER15A20>
2025-04-11 15:16:52 -0400

    Updated server to remain active, send proper file size

--- FlightServer/Src/server.cpp
+++ FlightServer/Src/server.cpp
@@ -8,7 +8,8 @@
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
-//Enum for server state machine
+
+// Enum for server state machine
 enum class ServerState {
     AWAITING_CONNECTION,
     CONNECTED,
@@ -63,13 +64,11 @@ namespace flight {
 int main() {
     WSADATA wsaData;
 
-    // Initialize WinSock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "[Server] WSAStartup failed!" << std::endl;
         return 1;
     }
 
-    // Create TCP socket
     SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == INVALID_SOCKET) {
         std::cerr << "[Server] Failed to create socket." << std::endl;
@@ -77,17 +76,14 @@ int main() {
         return 1;
     }
 
-    // Allow reuse of the address (for faster restarts)
     int opt = 1;
     setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt));
 
-    // Configure server address
     sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(12345);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind socket
     if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) != 0) {
         std::cerr << "[Server] Bind failed!" << std::endl;
         closesocket(serverSocket);
@@ -95,7 +91,6 @@ int main() {
         return 1;
     }
 
-    // Start listening
     if (listen(serverSocket, SOMAXCONN) != 0) {
         std::cerr << "[Server] Listen failed!" << std::endl;
         closesocket(serverSocket);
@@ -105,14 +100,8 @@ int main() {
 
     std::cout << "[Server] Server started. Listening on port 12345...\n";
 
-
     ServerState state = ServerState::AWAITING_CONNECTION;
-    SOCKET clientSocket = INVALID_SOCKET;
-    sockaddr_in clientAddr{};
-    int clientLen = sizeof(clientAddr);
 
-
-    // Accept and handle clients in a loop
     while (true) {
         sockaddr_in clientAddr{};
         int clientLen = sizeof(clientAddr);
@@ -122,22 +111,16 @@ int main() {
             std::cerr << "[Server] Accept failed!" << std::endl;
             continue;
         }
+
         std::cout << "[Server] Client connected.\n";
-        ServerState state = ServerState::CONNECTED;
+        state = ServerState::CONNECTED;
 
-
-
-        // Loop to receive commands from this client
         while (state != ServerState::DISCONNECTED) {
             switch (state) {
-            case ServerState::CONNECTED: {
-                // Send a welcome message upon connection
-                const char* welcomeMsg = "Connected to server. Awaiting commands.";
-                send(clientSocket, welcomeMsg, (int)strlen(welcomeMsg), 0);
-                // Transition to expecting commands.
+            case ServerState::CONNECTED:
                 state = ServerState::EXPECTING_COMMAND;
                 break;
-            }
+
             case ServerState::EXPECTING_COMMAND: {
                 char commandBuffer[256] = {};
                 int cmdLen = recv(clientSocket, commandBuffer, sizeof(commandBuffer), 0);
@@ -160,22 +143,24 @@ int main() {
                 else if (command == "QUIT") {
                     std::cout << "[Server] Client requested disconnect.\n";
                     state = ServerState::DISCONNECTED;
-                    break;
                 }
                 else {
                     std::cerr << "[Server] Unknown command received.\n";
                 }
                 break;
             }
+
             default:
                 break;
             }
         }
-        closesocket(serverSocket);
-        WSACleanup();
-        return 0;
+
+        closesocket(clientSocket);
+        std::cout << "[Server] Client disconnected.\n\n";
+        state = ServerState::AWAITING_CONNECTION;
     }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
 }
-    
-
-
